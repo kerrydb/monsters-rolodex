93ab01ea4d9080b4340e35bb6e9014f05fbf99e6
Kerry Bottoms <kerry@wigwom.com>
2019-11-04 12:59:04 -0600

    Updates

---
+++ precache-manifest.a798e3b2630fdf9bb3d37d8508da6a11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0e9de7e7d9989b520022cf0e60daa0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3e09ab892b4dcb5e750c",
+    "url": "/monsters-rolodex/static/css/main.1d9e7546.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "3e09ab892b4dcb5e750c",
+    "url": "/monsters-rolodex/static/js/main.bddc047b.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
